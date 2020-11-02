3d7531f824d8e6fd7ef8d52d865133746080ff2f
Jyrkinen Kalle <jyrkink1@lyta.org.aalto.fi>
2020-11-02 20:26:40 +0200

    Modified Vec3 a bit

---
+++ src/Vec3.cpp
@@ -0,0 +1,61 @@
+#include "Vec3.hpp"
+#include <cmath>
+
+float Vec3::X() const { return x_; }
+float Vec3::Y() const { return y_; }
+float Vec3::Z() const { return z_; }
+
+void Vec3::Normalize() {
+  float len_inv = 1 / sqrt(x_*x_ + y_*y_ + z_*z_);
+  x_ *= len_inv;
+  y_ *= len_inv;
+  z_ *= len_inv;
+}
+  
+Vec3 Vec3::operator + (const Vec3& v) const {
+  return Vec3(x_ + v.X(), y_ + v.Y(), z_ + v.Z());
+}
+Vec3 Vec3::operator + (float c) const {
+  return Vec3(c + x_, c + y_, c + z_);
+}
+Vec3 operator + (float c, const Vec3& v) {
+  return Vec3(c + v.X(), c + v.Y(), c + v.Z());
+}
+
+Vec3 Vec3::operator - (const Vec3& v) const {
+  return Vec3(x_ - v.X(), y_ - v.Y(), z_ - v.Z());
+}
+Vec3 Vec3::operator - (float c) const {
+  return Vec3(c - x_, c - y_, c - z_);
+}
+Vec3 operator - (float c, const Vec3& v) {
+  return Vec3(c - v.X(), c - v.Y(), c - v.Z());
+}
+
+Vec3 Vec3::operator * (const Vec3& v) const {
+  return Vec3(x_ * v.X(), y_ * v.Y(), z_ * v.Z());
+}
+Vec3 Vec3::operator * (float c) const {
+  return Vec3(c * x_, c * y_, c * z_);
+}
+Vec3 operator * (float c, const Vec3& v) {
+  return Vec3(c * v.X(), c * v.Y(), c * v.Z());
+}
+
+Vec3 Vec3::operator / (const Vec3& v) const {
+  return Vec3(x_ / v.X(), y_ / v.Y(), z_ / v.Z());
+}
+Vec3 Vec3::operator / (float c) const {
+  return Vec3(c / x_, c / y_, c / z_);
+}
+Vec3 operator / (float c, const Vec3& v) {
+  return Vec3(c / v.X(), c / v.Y(), c / v.Z());
+}
+
+
+float Vec3::DotProduct(Vec3& v) {
+  return x_*v.X() + y_*v.Y() + z_*v.Z();
+}
+Vec3 Vec3::CrossProduct(Vec3& v) {
+  return Vec3(y_*v.Z() - z_*v.Y(), z_*v.X() - x_*v.Z(), x_*v.Y() - y_*v.X());
+}
--- src/Vec3.hpp
+++ src/Vec3.hpp
@@ -1,29 +1,43 @@
-#include <cmath>
+class Vec3 {
 
+public:
 
-struct Vec3 {
+  Vec3(float x, float y, float z) : x_(x), y_(y), z_(z) { }
 
-    double x_, y_, z_;
+  float X() const;
+  float Y() const;
+  float Z() const;
 
-    Vec3(double x, double y, double z) : x_(x), y_(y), z_(z) { }
+  void Normalize();
+  
+  // For the operators, let's define 3 versions of each so that
+  // it supports operations with two Vec3 objects as well as operations
+  // with one Vec3 object and a float, in either order.
+  // We must declare the last one as a friend (see more:
+  // https://stackoverflow.com/questions/4622330/
+  // operator-overloading-member-function-vs-non-member-function)
+  
+  Vec3 operator + (const Vec3& v) const; // Vec3 + Vec3
+  Vec3 operator + (float c) const; // Vec3 + float
+  friend Vec3 operator + (float c, const Vec3& v); // float + Vec3
 
-    Vec3 operator + (const Vec3& vector) const { return Vec3(x_ + vector.x_, y_ + vector.y_, z_ + vector.z_); }
-    Vec3 operator - (const Vec3& vector) const { return Vec3(x_ - vector.x_, y_ - vector.y_, z_ - vector.z_); }
-    Vec3 operator * (double constant) const { return Vec3(constant * x_, constant * y_, constant * z_); }
-    Vec3 operator / (double constant) const { return Vec3(x_ / constant, y_ / constant, z_ / constant); }
+  Vec3 operator - (const Vec3& v) const;
+  Vec3 operator - (float c) const;
+  friend Vec3 operator - (float c, const Vec3& v);
 
-    Vec3 normalize() const {
-        double VectorLength = sqrt(x_ * x_ + y_ * y_ + z_ * z_);
-        return Vec3(x_ / VectorLength, y_ / VectorLength, z_ / VectorLength);
-    }
+  Vec3 operator * (const Vec3& v) const;
+  Vec3 operator * (float c) const;
+  friend Vec3 operator * (float c, const Vec3& v);
 
-    double dot_product(const Vec3& vector) {
-        return (x_ * vector.x_ + y_ * vector.y_ + z_ * vector.z_);
-    }
+  Vec3 operator / (const Vec3& v) const;
+  Vec3 operator / (float c) const;
+  friend Vec3 operator / (float c, const Vec3& v);
 
-    Vec3 cross_product(const Vec3& vector) {
-        return Vec3(y_ * vector.z_ - z_ * vector.y_, z_ * vector.x_ - x_ * vector.z_, x_ * vector.y_ - y_ * vector.x_);
-    }
-
+  float DotProduct(Vec3& v);
+  Vec3 CrossProduct(Vec3& v);
+  
+private:
+  
+    float x_, y_, z_;
+  
 };
-
