2bd4138b6f9a565b7dbfb60f9d7bdf351bf90bae
ikaheia3 <atso.ikaheimo@aalto.fi>
2020-11-03 15:02:30 +0200

    fixed camera

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -9,23 +9,23 @@
 //a Camera is created with a defined resolution, can't be changed
 //is now constructed e.g. Camera<10000>(direction, x_reso, y_reso, x_angle, y_angle)
 //This is not robust, must be addressed in main function.
-template < int N_RAYS > 
+//template < int N_RAYS > 
 class Camera{
 public:
     Camera(Path& direction, size_t x_reso, size_t y_reso, float x_angle, float y_angle)
-    : direction_(direction), x_reso_(x_reso), y_reso_(y_reso), x_angle_(x_angle), y_angle_(y_angle) {
-        rays_& = Ray& rays_ [N_RAYS];
-    }
+    : direction_(direction), x_reso_(x_reso), y_reso_(y_reso), x_angle_(x_angle), y_angle_(y_angle) {}
 
-    void GetImage(&Environment env);
+    void GetImage(Environment& env);
     
     void SetAngles(float x, float y);
-    void SetDirection(&Path);
+    void SetDirection(Path& p);
 
     std::ostream PrintInfo(std::ostream os);
 
+    std::string Time();
+
 private:
-    std::array<Ray&, N_RAYS>& rays_;
+//    std::array<Ray&, N_RAYS>& rays_;
 
     Path& direction_;
     size_t x_reso_;
