65faf63c9db86d238ecd66ff30430b4ebedad113
Anton <anton.pirhonen@aalto.fi>
2020-11-08 15:59:46 +0200

    Create simple triangle parser

--- src/Environment.cpp
+++ src/Environment.cpp
@@ -1,6 +1,9 @@
 #include "Environment.hpp"
+#include "Triangle.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include "Vec3.hpp"
 
 Environment::Environment() { }
 
@@ -17,10 +20,26 @@ void Environment::PrintInfo() const {
 }
 
 void Environment::LoadEnvironment(std::string path) {
-    std::ifstream istream(path);
-    while (!istream.eof()) {
-        std::string line;
-        std::getline(istream, line);
-        std::cout << line << std::endl;
+    std::ifstream file (path);
+    if (!file.is_open()) {
+        std::cout << ".obj file " << path <<" not found" << std::endl;
     }
+
+    std::string line;
+    std::vector<Vec3> vertices;
+    Material material = Material();
+    while (std::getline(file, line)) {
+        if (line.substr(0, 2)== "v "){
+            std::istringstream iss(line.substr(2));
+            double x, y, z;
+            iss>>x;iss>>y;iss>>z;
+            vertices.push_back(Vec3(x,y,z));
+        } else if (line.substr(0, 2)== "f ") {
+            std::istringstream iss(line.substr(2));
+            int i, j, k;
+            iss>>i;iss>>j;iss>>k;
+            bodies_.push_back(new Triangle(material, vertices.at(i-1), vertices.at(j-1), vertices.at(k-1)));
+        }
+    }
+
 }
