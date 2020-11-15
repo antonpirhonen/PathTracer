98815673380cf0db5b885c5c606eb304d6885485
Joona Munukka <joona.munukka@aalto.fi>
2020-11-15 18:26:16 +0200

    Fixes and updates to Random.hpp

--- src/Random.hpp
+++ src/Random.hpp
@@ -7,66 +7,77 @@
 #include <iostream>
 #include "Vec3.hpp"
 
-float randomNumber() {
-    static std::mt19937 generator(std::time(0));
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    return distribution(generator);
-}
+namespace Random {
 
+    // returns random number thats in [0, 1)
+    float randomNumber() {
+        static std::mt19937 generator(std::time(0));
+        static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+        return distribution(generator);
+    }
 
-float randomNumber(float min, float max) {
-    static std::mt19937 generator(std::time(0));
-    static std::uniform_real_distribution<float> distribution(min, max);
-    return distribution(generator);
-}
+    // returns random number thats in [min, max)
+    float randomNumber(float min, float max) {
+        static std::mt19937 generator(std::time(0));
+        static std::uniform_real_distribution<float> distribution(min, max);
+        return distribution(generator);
+    }
 
-Vec3 RandomVector() {
-    return Vec3(randomNumber(), randomNumber(), randomNumber());
-}
+    // returns random vector where X, Y, Z each are random numbers in [0, 1)
+    Vec3 RandomVector() {
+        return Vec3(randomNumber(), randomNumber(), randomNumber());
+    }
 
-Vec3 RandomVector(float min, float max) {
-    return Vec3(randomNumber(min, max), randomNumber(min, max), randomNumber(min, max));
-}
+    // returns random vector where X, Y, Z each are random numbers in [min, max)
+    Vec3 RandomVector(float min, float max) {
+        return Vec3(randomNumber(min, max), randomNumber(min, max), randomNumber(min, max));
+    }
 
-Vec3 RandomVectorInXDegreeFromOther(Vec3 vector, double degree) {
+    //returns random vector atleast "degree" degrees away from given vector "vector"
+    Vec3 RandomVector(Vec3 vector, float degree) {
 
-    Vec3 givenVector = vector;
-    givenVector.Normalize();
+        auto angle = degree * 180 / M_PI;
+        auto phi_lower = M_PI - angle;
+        auto theta = 2 * M_PI * randomNumber();
+        auto phi = acos(1 - (cos(phi_lower) + 1) * randomNumber());
+        Vec3 randomVector = Vec3(sin(phi) * cos(theta), sin(phi) * sin(theta), cos(phi));
 
-    while (true) {
+        auto a1 = atan2(sqrt(vector.X() * vector.X() + vector.Y() * vector.Y()), vector.Z());
+        auto a2 = atan2(vector.Y(), vector.X());
 
-        Vec3 randomNormalVector = RandomVector(-1, 1);
+        double R[3][3] = {(cos(a1) * cos(a2), -sin(a2), sin(a1) * cos(a2)),
+                        (cos(a1) * sin(a2), cos(a2), sin(a1) * sin(a2)),
+                        (-sin(a1), 0, cos(a1))};
 
-        randomNormalVector.Normalize();
+        return Vec3((randomVector.X() * R[0][0] + randomVector.Y() * R[1][0] + randomVector.Z() * R[2][0]),
+                    (randomVector.X() * R[0][1] + randomVector.Y() * R[1][1] + randomVector.Z() * R[2][1]),
+                    (randomVector.X() * R[0][2] + randomVector.Y() * R[1][2] + randomVector.Z() * R[2][2]));
+
+    }
+
+    //returns random vector from unit sphere
+    Vec3 RandomVectorInUnitSpehere() {
+
+        auto theta = 2 * M_PI * randomNumber();
+        auto phi = acos(1 - 2 * randomNumber());
+        return Vec3(sin(phi) * cos(theta), sin(phi) * sin(theta), cos(phi));
         
-        if (acos(randomNormalVector.DotProduct(givenVector)) * 180 / M_PI < degree) continue;
-        return randomNormalVector;
-
     }
 
-}
+    //returns random vector from same hemispehere with given "normal" vector
+    Vec3 RandomVectorInHemisphere(Vec3 normal) {
 
-Vec3 RandomVectorInUnitSpehere() {
+        Vec3 vector = RandomVectorInUnitSpehere();
 
-    while (true) {
-        Vec3 vector = RandomVector(-1,1);
-        if (vector.DotProduct(vector) >= 1) continue;
-        return vector;
+        if (normal.DotProduct(vector) > 0.0)
+        {
+            return vector;
+        } else {
+            return -1.0 * vector;
+        }
+        
+
     }
-    
-}
-
-Vec3 RandomVectorInHemisphere(Vec3 normal) {
-
-    Vec3 vector = RandomVectorInUnitSpehere();
-
-    if (normal.DotProduct(vector) > 0.0)
-    {
-        return vector;
-    } else {
-        return -1.0 * vector;
-    }
-    
 
 }
 
