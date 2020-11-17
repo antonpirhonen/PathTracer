7b4c3c5bb57d18d844c8addfc25db715eec70371
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-11-17 13:49:04 +0200

    Test for transparent material

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,15 @@
 #include "CameraSimple.hpp"
 #include "MaterialSpecular.hpp"
 #include "MaterialDiffuse.hpp"
+#include "MaterialTransparent.hpp"
 
 int main() {
     Environment env = Environment();
     env.PrintInfo();
     MaterialDiffuse diffuse = MaterialDiffuse(false, RGB_color{0,0,0}, Color_removal{1,0,0}, 0.01);
-    MaterialSpecular specular = MaterialSpecular(false, RGB_color{0,0,0}, Color_removal{1,0,0});
+    MaterialSpecular specular = MaterialSpecular(false, RGB_color{0,0,0}, Color_removal{1,1,0});
     MaterialSpecular radiant = MaterialSpecular(true, RGB_color{255,255,255}); 
-
+    MaterialTransparent transparent = MaterialTransparent(false, RGB_color{0,0,0}, Color_removal{1,0,0}, 2);
     //Kaksi yksinkertaista testiä
     //Ensimmäinen
 //    Triangle spec_tr = Triangle(specular, Vec3{1,-1,1}, Vec3{1,1,-1}, Vec3{1,1,1});
@@ -26,6 +27,7 @@ int main() {
     CameraSimple cam = CameraSimple(Vec3{0,0,0}, Vec3{4,-2,2}, Vec3{4,2,-2}, 1000, 1000);
     Triangle spec_tr = Triangle(diffuse, Vec3{4,-2,2}, Vec3{6,-2,-3}, Vec3{6,3,2});
     Triangle rad_tr = Triangle(radiant, Vec3{4,1,-1}, Vec3{4,1,0}, Vec3{4,0,-1});
+    Triangle trans_tr = Triangle(transparent, Vec3{0,1,-1}, Vec3{4,1,0}, Vec3{4,0,-1});
 
 //    env.AddBody(&rad_tr);
     env.AddBody(&spec_tr);
