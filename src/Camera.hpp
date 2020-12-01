9da656a4ba85a61e750d55619f0b82d661687939
ikaheia3 <atso.ikaheimo@aalto.fi>
2020-12-01 12:55:26 +0200

    Working and tested final camera

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -9,10 +9,10 @@
 
 class Camera{
 public:
-    Camera(Path& direction, size_t x_reso = 1000, size_t y_reso = 1000, float x_angle = 45, float y_angle = 45)
-    : path_(direction), x_reso_(x_reso), y_reso_(y_reso), x_angle_(x_angle), y_angle_(y_angle) {}
+    Camera(Vec3 origin, Vec3 direction, size_t x_reso = 1000, size_t y_reso = 1000, float x_angle = 45, float y_angle = 45)
+    : origin_(origin),direction_(direction),  x_reso_(x_reso), y_reso_(y_reso), x_angle_(x_angle), y_angle_(y_angle) {}
 
-    void GetImage(Environment& env);
+    void GetImage(Environment& env, unsigned int spp);
     
     void SetAngles(float x, float y);
     void SetDirection(Path& p);
@@ -22,7 +22,8 @@ public:
     std::string Time();
 
 private:
-    Path& path_;
+    Vec3 direction_;
+    Vec3 origin_;
     const size_t x_reso_;
     const size_t y_reso_; 
     float x_angle_;
