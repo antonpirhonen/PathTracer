0431258c59971a72ae09c706ab9c57a49c2a88b9
Anton <anton.pirhonen@aalto.fi>
2020-12-08 16:00:24 +0200

    Validate sample size in main

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,17 @@ int main(int argc, char* argv[]) {
 	int samples_per_pixel = 1;
 
 	if (argc > 2) {
-	    std::string s = std::string(argv[2]);
-	    samples_per_pixel = std::stoi(s);
+		std::string s = std::string(argv[2]);
+		try {
+			samples_per_pixel = std::stoi(s);
+		} catch (std::invalid_argument& e) {
+			std::cout << "Given sample size not convertible to int. Using sample size of 1." << std::endl;
+		}
+	}
+
+	if (samples_per_pixel < 1) {
+		std::cout << "Sample size must be greater or equal to 1. Using sample size of 1." << std::endl;
+		samples_per_pixel = 1;
 	}
 
 	std::cout << "Rendering an image with " << samples_per_pixel
@@ -21,7 +30,7 @@ int main(int argc, char* argv[]) {
 		scene.DrawImageFrom(filename, samples_per_pixel);
 	} catch (std::invalid_argument& e) {
 		std::cout << "Invalid input: " << e.what() << std::endl;
-	} catch (nlohmann::detail::type_error e) {
+	} catch (nlohmann::detail::type_error& e) {
 		std::cout << "Error while parsing JSON: " << e.what() << std::endl;
 	} catch (std::out_of_range& oor) {
 		std::cout << "Index out of range: " << oor.what() << std::endl;
